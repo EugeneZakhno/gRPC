6402db6ce6b3d476aac085c9893bae8a499967ca
EugeneZakhno <zohan1234567@gmail.com>
2021-06-06 17:35:47 +0300

    initial

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import com.example.grpc.GreatingService;
+import com.example.grpc.GreetingServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class Client {
+    public static void main(String[] args) {
+        ManagedChannel channel = ManagedChannelBuilder.forTarget("localhost:8080")
+        .usePlaintext().build();
+
+        GreetingServiceGrpc.GreetingServiceBlockingStub stub = GreetingServiceGrpc.newBlockingStub(channel);
+        GreatingService.HelloRequest request = GreatingService.HelloRequest
+                .newBuilder().setText("I am who knocks!").build();
+        GreatingService.HelloResponse response = stub.knockKnock(request);
+        System.out.println(response);
+        channel.shutdownNow();
+    }
+}
